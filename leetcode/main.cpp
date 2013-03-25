3d45b52a3f1a6f257ad755e8b70e57c183fdf2c9
wenzhuman <manwenzhu@gmail.com>
2013-03-25 14:57:35 -0400

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Wenzhu Man on 2013-03-25.
+//  Copyright (c) 2013 Wenzhu Man. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
