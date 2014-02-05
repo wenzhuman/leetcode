f4f94f30095c5ed96eaa24fb38a13af76af37eab
wenzhu man <wman@uwaterloo.ca>
2014-02-04 23:20:40 -0500

    refactor unique path

--- UniquePath.cpp
+++ UniquePath.cpp
@@ -3,9 +3,7 @@ public:
     int uniquePaths(int m, int n) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
-        //vector<vector<int> > a(m+1,vector<int>(n+1,0));
-        auto a = new int[m+1][n+1];
+        int a[m+1][n+1];
         
         for(int i = 0;i<=m;i++){
             for(int j = 0;j<=n;j++){
@@ -21,15 +19,5 @@ public:
             }
         }
         return a[0][0];
-        /*const int M_MAX = 100;
-        const int N_MAX = 100;
-        int mat[M_MAX+2][N_MAX+2] = {0};
-        mat[m][n+1] = 1;
- 
-        for (int r = m; r >= 1; r--)
-        for (int c = n; c >= 1; c--)
-        mat[r][c] = mat[r+1][c] + mat[r][c+1];
- 
-        return mat[1][1];*/
     }
 };
